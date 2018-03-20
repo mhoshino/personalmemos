a041cba1d316140c6c9e95f09ee08988af35ad17
Machi Hoshino <mhoshino@mirantis.com>
2018-03-20 12:24:48 +0900

    fix

---
+++ contrail-kolla-kubernetes/common.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -x
+echo "control_ip  stacknamecontrol_hostname" >> /etc/hosts
+ssh-keyscan localhost >> /root/.ssh/known_hosts
+ssh-keyscan stacknamecontrol_hostname >> /root/.ssh/known_hosts
+ssh-keyscan 127.0.0.1 >> /root/.ssh/known_hosts
+ssh-keyscan control_ip >> /root/.ssh/known_hosts
+i=0
+while true
+do
+   if [ $i -ge servercount ]
+   then
+     break
+   fi
+   echo "compute_ip$i stacknamecompute_hostname$i" >> /etc/hosts
+   ssh-keyscan stacknamecompute_hostname$i >> /root/.ssh/known_hosts
+   ssh-keyscan compute_ip$i >> /root/.ssh/known_hosts
+   i=$((i+1))
+done
+apt-get install -y python docker docker.io
+python -c 'import json; obj={}; obj["bip"]="docker_bridge_ip"; print(json.dumps(obj))' > /etc/docker/daemon.json
+systemctl restart docker
+apt-get install --no-install-recommends -y git
+git clone https://git.openstack.org/openstack/openstack-helm-infra.git /opt/openstack-helm-infra
+git clone https://git.openstack.org/openstack/openstack-helm.git /opt/openstack-helm
---
+++ contrail-kolla-kubernetes/gluster.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+set -x
+
+if [ `hostname` = "stacknamecontrol_hostname" ]
+then
+i=0
+whereis kubectl || exit 1
+
+while true
+do
+   if [ $i -ge servercount ]
+   then
+     break
+   fi
+   kubectl label node stacknamecompute_hostname${i} storagenode=glusterfs
+   i=$((i+1))
+done
+HEKETI_BIN="heketi-cli"      # heketi or heketi-cli
+HEKETI_VERSION="6.0.0"       # latest heketi version => https://github.com/heketi/heketi/releases
+HEKETI_OS="linux"            # linux or darwin
+
+
+unset HEKETI_BIN HEKETI_VERSION HEKETI_OS
+
+git clone https://github.com/heketi/heketi
+( cd heketi/extras/kubernetes; kubectl create -f glusterfs-daemonset.json ; kubectl create -f heketi-service-account.json; kubectl create clusterrolebinding heketi-gluster-admin --clusterrole=edit --serviceaccount=default:heketi-service-account; kubectl create secret generic heketi-config-secret --from-file=./heketi.json; kubectl create -f heketi-bootstrap.json )
+
+
+
+HEKETI_BIN="heketi-cli"      # heketi or heketi-cli
+HEKETI_VERSION="6.0.0"       # latest heketi version => https://github.com/heketi/heketi/releases
+HEKETI_OS="linux"            # linux or darwin
+curl -SL https://github.com/heketi/heketi/releases/download/v${HEKETI_VERSION}/heketi-v${HEKETI_VERSION}.${HEKETI_OS}.amd64.tar.gz -o /tmp/heketi-v${HEKETI_VERSION}.${HEKETI_OS}.amd64.tar.gz && \
+tar xzvf /tmp/heketi-v${HEKETI_VERSION}.${HEKETI_OS}.amd64.tar.gz -C /tmp && \
+rm -vf /tmp/heketi-v${HEKETI_VERSION}.${HEKETI_OS}.amd64.tar.gz && \
+cp /tmp/heketi/${HEKETI_BIN} /usr/local/bin/${HEKETI_BIN}_${HEKETI_VERSION} && \
+rm -vrf /tmp/heketi && \
+cd /usr/local/bin && \
+ln -vsnf ${HEKETI_BIN}_${HEKETI_VERSION} ${HEKETI_BIN} && cd
+
+HEKETI_IP=`kubectl get services deploy-heketi -o json | jq .spec.clusterIP -r`
+HEKETI_PORT=`kubectl get services deploy-heketi -o json | jq .spec.ports[0].port -r`
+export HEKETI_CLI_SERVER=http://${HEKETI_IP}:${HEKETI_PORT}
+python -c 'import json
+obj={}
+obj["clusters"]=[]
+obj["clusters"].append(0)
+obj["clusters"][0]={}
+obj["clusters"][0]["nodes"]=[]
+
+for i in range(0,servercount):
+  obj["clusters"][0]["nodes"].append(i)
+  obj["clusters"][0]["nodes"][i]={}
+  obj["clusters"][0]["nodes"][i]["node"]={}
+  obj["clusters"][0]["nodes"][i]["node"]["hostnames"]={}
+  obj["clusters"][0]["nodes"][i]["node"]["hostnames"]["manage"]=[]
+  obj["clusters"][0]["nodes"][i]["node"]["hostnames"]["manage"].append(0)
+  obj["clusters"][0]["nodes"][i]["node"]["hostnames"]["manage"][0]="stacknamecompute_hostname"+str(i)
+  obj["clusters"][0]["nodes"][i]["node"]["hostnames"]["storage"]=[]
+  obj["clusters"][0]["nodes"][i]["node"]["hostnames"]["storage"].append(0)
+  obj["clusters"][0]["nodes"][i]["node"]["hostnames"]["storage"][0]="compute_ip"+str(i)
+  obj["clusters"][0]["nodes"][i]["node"]["zone"]=1
+  obj["clusters"][0]["nodes"][i]["devices"]=[]
+  obj["clusters"][0]["nodes"][i]["devices"].append(0)
+  obj["clusters"][0]["nodes"][i]["devices"][0]="gluster_device"
+print(json.dumps(obj))' > gluster_top.json
+
+heketi-cli topology load --json=gluster_top.json
+fi
+
+
+

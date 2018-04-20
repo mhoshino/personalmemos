527ed5116df279377cadce5437a263599df1c9c7
Machi Hoshino <mhoshino@mirantis.com>
2018-04-20 11:43:09 +0900

    update

--- mcp/common.sh
+++ mcp/common.sh
@@ -11,19 +11,18 @@ systemctl restart docker
 apt-get install --no-install-recommends -y git
 apt-get install git vim tree python-pip -y
 pip install --upgrade pip
-apt-get install reclass salt-master -y
 apt-get install salt-common salt-minion -y
 
-cat > /etc/apt/sources.list.d/mcp_salt.list << EOF
-deb [arch=amd64] http://apt-mk.mirantis.com/xenial stable salt
-EOF
-wget -qO - http://apt-mk.mirantis.com/public.gpg | sudo apt-key add -
 
 cat > /etc/salt/minion.d/minion.conf << EOF
 master: stacknamecfg_hostname
 EOF
+
 if [ `hostname` = "stacknamecfg_hostname" ]
 then
+
+  # Install and config(temporary) salt master
+  apt-get install reclass salt-master -y
 cat > /etc/salt/master.d/master.conf <<EOF
 file_roots:
   base:
@@ -42,6 +41,7 @@ ext_pillar:
 master_tops:
   reclass: *reclass
 EOF
+  # Install and config reclass
   systemctl restart salt-master
   mkdir /etc/reclass
 cat > /etc/reclass/reclass-config.yml <<EOF
@@ -50,17 +50,39 @@ pretty_print: True
 output: yaml
 inventory_base_uri: /srv/salt/reclass
 EOF
+
+  # Make base directories
   mkdir -p /srv/salt/reclass/nodes
   mkdir -p /srv/salt/reclass/classes
 
-  reclass --inventory
+  # Install basic salt-formulas
+  apt-get install reclass salt-master -y
+cat > /etc/apt/sources.list.d/mcp_salt.list << EOF
+deb [arch=amd64] http://apt-mk.mirantis.com/xenial stable salt
+EOF
+  wget -qO - http://apt-mk.mirantis.com/public.gpg | sudo apt-key add -
   apt-get update
-  apt-get install salt-formula-reclass salt-formula-salt -y --allow-unauthenticated
-  ln -s "/usr/share/salt-formulas/reclass/service" /srv/salt/reclass/classes/
-  git clone https://github.com/Mirantis/reclass-system-salt-model.git
-  mkdir -p /srv/salt/reclass/classes/system
-  cp -R /reclass-system-salt-model/* /srv/salt/reclass/classes/system/
+  
+  apt-get install salt-formula-linux salt-formula-git salt-formula-reclass salt-formula-salt salt-formula-openssh -y --allow-unauthenticated
+
+  # Create service level reclass
+  mkdir -p /srv/salt/reclass/classes/service
+  for pkg in linux git reclass salt openssh
+  do
+    ln -s /usr/share/salt-formulas/reclass/service/$pkg /srv/salt/reclass/classes/service/$pkg
+  done
+
+  # Create system level reclass
+  git clone https://github.com/Mirantis/reclass-system-salt-model.git /srv/salt/reclass/classes/system
+  
+  # Create cluster level reclass
   mkdir -p /srv/salt/reclass/classes/cluster
+
+  # Create environment
+  mkdir -p /src/salt/env
+  ln -s /usr/share/salt-formulas/env /src/salt/env/prd
+
+  # Create salt config for master node
 cat > /srv/salt/reclass/nodes/stacknamecfg_hostname.local.yml <<EOF
 classes:
 - cluster.stackname.infra.config

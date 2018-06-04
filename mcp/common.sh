5f92ff53efce695d6d9bdd90cb4f401d14e130aa
mhoshino <mhoshino@mirantis.com>
2018-06-04 05:44:04 +0000

    add oc

--- mcp/common.sh
+++ mcp/common.sh
@@ -3,8 +3,8 @@ sh -x
 if [ `hostname` = "stacknamecfg_hostname" ]
 then
   curl -O https://raw.githubusercontent.com/mhoshino/simplemcp/master/install_salt_master.sh
-  sh -x ./install_salt_master.sh stackname ubuntuversion ubuntucode saltversion systemclassversion
+  sh -x ./install_salt_master.sh stackname ubuntu_version ubuntu_code salt_version system_class_version
 else
   curl -O https://raw.githubusercontent.com/mhoshino/simplemcp/master/install_salt_minion.sh
-  sh -x ./install_salt_minion.sh cfg_ip ubuntuversion ubuntucode saltversion
+  sh -x ./install_salt_minion.sh cfg_ip ubuntu_version ubuntu_code salt_version
 fi
